25b4314a8d03c38e49036221795acf167dfd1814
Hamza-160804 <raohumza52@gmail.com>
2025-02-12 21:39:24 +0500

    update

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,3 @@
 module.exports = {
-  {
-    test: /\.css$/,
-    use: [
-      "style-loader",  // Injects CSS into DOM
-      "css-loader",    // Parses CSS
-      {
-        loader: "postcss-loader", // Processes CSS with PostCSS plugins
-        options: {
-          postcssOptions: {
-            plugins: ["tailwindcss", "autoprefixer"],
-          },
-        },
-      },
-    ],
-  }
-  
+  plugins: ["tailwindcss", "autoprefixer"]
 };
